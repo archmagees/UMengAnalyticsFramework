d3bb78b9bf4fb6b31cdbdcd3de498101b82b8160
Arthur <archmagees.dev@gmail.com>
2018-05-03 14:22:02 +0800

    Add swift file and a objective-c bridging header in unit test target.

---
+++ UMengAnalyticsFrameworkTests/UseSwiftModuleRequire.swift
@@ -0,0 +1,9 @@
+//
+//  UseSwiftModuleRequire.swift
+//  UMengAnalyticsFrameworkTests
+//
+//  Created by archmagees on 2018/05/03.
+//  Copyright © 2018 archmagees. All rights reserved.
+//
+
+import Foundation
