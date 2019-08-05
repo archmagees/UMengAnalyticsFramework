1bf2f74b298817d1f606b2ca9e6d5f6e4b2b51e6
Arthur <archmagees.dev@gmail.com>
2019-08-05 16:13:07 +0800

    🔖 [Release] adapt for iOS 13 and upgrade to 6.0.5

--- UMengAnalyticsFramework/MobClick.h
+++ UMengAnalyticsFramework/MobClick.h
@@ -7,16 +7,14 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
-typedef void(^CallbackBlock)();
+typedef void(^CallbackBlock)(void);
 
 /**
- 统计的场景类别，默认为普通统计；若使用游戏统计API，则需选择游戏场景类别，如E_UM_GAME。
+ 统计的场景类别，默认为普通统计；若使用游戏统计API，则需选择游戏场景类别，如E_UM_NORMAL。
  */
 typedef NS_ENUM (NSUInteger, eScenarioType)
 {
     E_UM_NORMAL = 0,    // default value
-    E_UM_GAME   = 1,    // game
-    E_UM_DPLUS  = 4    // DPlus
 };
 
 @class CLLocation;
@@ -29,14 +27,12 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
 ///---------------------------------------------------------------------------------------
 
 /** 设置 统计场景类型，默认为普通应用统计：E_UM_NORMAL
- @param 游戏统计必须设置为：E_UM_GAME.
- @return void.
+ @param eSType eScenarioType
  */
 + (void)setScenarioType:(eScenarioType)eSType;
 
 /** 开启CrashReport收集, 默认YES(开启状态).
  @param value 设置为NO,可关闭友盟CrashReport收集功能.
- @return void.
 */
 + (void)setCrashReportEnabled:(BOOL)value;
 
@@ -48,7 +44,6 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
 /** 手动页面时长统计, 记录某个页面展示的时长.
  @param pageName 统计的页面名称.
  @param seconds 单位为秒，int型.
- @return void.
  */
 + (void)logPageView:(NSString *)pageName seconds:(int)seconds;
 
@@ -56,7 +51,6 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
  使用方法：必须配对调用beginLogPageView:和endLogPageView:两个函数来完成自动统计，若只调用某一个函数不会生成有效数据。
  在该页面展示时调用beginLogPageView:，当退出该页面时调用endLogPageView:
  @param pageName 统计的页面名称.
- @return void.
  */
 + (void)beginLogPageView:(NSString *)pageName;
 
@@ -64,7 +58,6 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
  使用方法：必须配对调用beginLogPageView:和endLogPageView:两个函数来完成自动统计，若只调用某一个函数不会生成有效数据。
  在该页面展示时调用beginLogPageView:，当退出该页面时调用endLogPageView:
  @param pageName 统计的页面名称.
- @return void.
  */
 + (void)endLogPageView:(NSString *)pageName;
 
@@ -77,13 +70,12 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
 使用前，请先到友盟App管理后台的设置->编辑自定义事件 中添加相应的事件ID，然后在工程中传入相应的事件ID
  
  @param  eventId 网站上注册的事件Id.
- @param  label 分类标签。不同的标签会分别进行统计，方便同一事件的不同标签的对比,为nil或空字符串时后台会生成和eventId同名的标签.
- @param  accumulation 累加值。为减少网络交互，可以自行对某一事件ID的某一分类标签进行累加，再传入次数作为参数。
- @return void.
  */
 + (void)event:(NSString *)eventId; //等同于 event:eventId label:eventId;
 /** 自定义事件,数量统计.
  使用前，请先到友盟App管理后台的设置->编辑自定义事件 中添加相应的事件ID，然后在工程中传入相应的事件ID
+ @param  eventId 网站上注册的事件Id.
+ @param  label 分类标签。不同的标签会分别进行统计，方便同一事件的不同标签的对比,为nil或空字符串时后台会生成和eventId同名的标签.
  */
 + (void)event:(NSString *)eventId label:(NSString *)label; // label为nil或@""时，等同于 event:eventId label:eventId;
 
@@ -99,11 +91,6 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
     beginEvent,endEvent要配对使用,也可以自己计时后通过durations参数传递进来
  
  @param  eventId 网站上注册的事件Id.
- @param  label 分类标签。不同的标签会分别进行统计，方便同一事件的不同标签的对比,为nil或空字符串时后台会生成和eventId同名的标签.
- @param  primarykey 这个参数用于和event_id一起标示一个唯一事件，并不会被统计；对于同一个事件在beginEvent和endEvent 中要传递相同的eventId 和 primarykey
- @param millisecond 自己计时需要的话需要传毫秒进来
- @return void.
- 
  @warning 每个event的attributes不能超过10个
     eventId、attributes中key和value都不能使用空格和特殊字符，必须是NSString,且长度不能超过255个字符（否则将截取前255个字符）
     id， ts， du是保留字段，不能作为eventId及key的名称
@@ -117,6 +104,8 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
 + (void)endEvent:(NSString *)eventId;
 /** 自定义事件,时长统计.
  使用前，请先到友盟App管理后台的设置->编辑自定义事件 中添加相应的事件ID，然后在工程中传入相应的事件ID.
+ @param  eventId 网站上注册的事件Id.
+ @param  label 分类标签。不同的标签会分别进行统计，方便同一事件的不同标签的对比,为nil或空字符串时后台会生成和eventId同名的标签.
  */
 
 + (void)beginEvent:(NSString *)eventId label:(NSString *)label;
@@ -137,6 +126,8 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
 + (void)endEvent:(NSString *)eventId primarykey:(NSString *)keyName;
 /** 自定义事件,时长统计.
  使用前，请先到友盟App管理后台的设置->编辑自定义事件 中添加相应的事件ID，然后在工程中传入相应的事件ID.
+ @param  eventId 网站上注册的事件Id.
+ @param millisecond 自己计时需要的话需要传毫秒进来
  */
 
 + (void)event:(NSString *)eventId durations:(int)millisecond;
@@ -152,18 +143,17 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
 
 
 #pragma mark - user methods
+
++ (void)profileSignInWithPUID:(NSString *)puid;
 /** active user sign-in.
  使用sign-In函数后，如果结束该PUID的统计，需要调用sign-Off函数
  @param puid : user's ID
  @param provider : 不能以下划线"_"开头，使用大写字母和数字标识; 如果是上市公司，建议使用股票代码。
- @return void.
  */
-+ (void)profileSignInWithPUID:(NSString *)puid;
 + (void)profileSignInWithPUID:(NSString *)puid provider:(NSString *)provider;
 
 /** active user sign-off.
  停止sign-in PUID的统计
- @return void.
  */
 + (void)profileSignOff;
 
@@ -175,13 +165,11 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
 /** 设置经纬度信息
  @param latitude 纬度.
  @param longitude 经度.
- @return void
  */
 + (void)setLatitude:(double)latitude longitude:(double)longitude;
 
 /** 设置经纬度信息
  @param location CLLocation 经纬度信息
- @return void
  */
 + (void)setLocation:(CLLocation *)location;
 
@@ -199,7 +187,6 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
 
 /** 设置 app secret
  @param secret string
- @return void.
  */
 + (void)setSecret:(NSString *)secret;
 
@@ -207,8 +194,41 @@ typedef NS_ENUM (NSUInteger, eScenarioType)
 
 /** DeepLink事件
  @param link 唤起应用的link
- @return void.
  */
 + (void)onDeepLinkReceived:(NSURL *)link;
 
+/**
+ * 设置预置事件属性 键值对 会覆盖同名的key
+ */
++(void) registerPreProperties:(NSDictionary *)property;
+
+/**
+ *
+ * 删除指定预置事件属性
+ */
++(void) unregisterPreProperty:(NSString *)propertyName;
+
+/**
+ * 获取预置事件所有属性；如果不存在，则返回空。
+ */
++(NSDictionary *)getPreProperties;
+
+/**
+ *清空所有预置事件属性。
+ */
++(void)clearPreProperties;
+
+
+/**
+ * 设置关注事件是否首次触发,只关注eventList前五个合法eventID.只要已经保存五个,此接口无效
+ */
++(void)setFirstLaunchEvent:(NSArray *)eventList;
+
+/** 设置是否自动采集页面, 默认NO(不自动采集).
+ @param value 设置为YES, umeng SDK 会将自动采集页面信息
+ */
++ (void)setAutoPageEnabled:(BOOL)value;
+
 @end
+
+
--- UMengAnalyticsFramework/UMengAnalyticsFramework.h
+++ UMengAnalyticsFramework/UMengAnalyticsFramework.h
@@ -16,7 +16,5 @@ FOUNDATION_EXPORT const unsigned char UMengAnalyticsFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <UMengAnalyticsFramework/PublicHeader.h>
 #import <UMengAnalyticsFramework/MobClick.h>
-#import <UMengAnalyticsFramework/MobClickGameAnalytics.h>
-#import <UMengAnalyticsFramework/DplusMobClick.h>
 #import <UMengAnalyticsFramework/UMConfigure.h>
 
